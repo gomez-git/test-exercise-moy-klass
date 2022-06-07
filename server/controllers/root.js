7524bc0b150dbb3788e9bb758afddfc686071873
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-07 11:55:22 +0300

    Update error message

--- server/controllers/root.js
+++ server/controllers/root.js
@@ -5,7 +5,7 @@ export default async (req, res) => {
     const obj = await rootService(req.query);
     res.json(obj);
   } catch (error) {
-    console.error(error.message); // eslint-disable-line no-console
-    res.status(400).send(error.message);
+    console.error(error.message, error.stack); // eslint-disable-line no-console
+    res.status(400).send(`Error -> ${error.message}`);
   }
 };
